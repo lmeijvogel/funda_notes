17aa3c7b61090d1fae42f0f7422ca02797cb618b
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2020-06-06 22:27:30 +0200

    First working version using plain JS.

--- index.js
+++ index.js
@@ -1,27 +1,81 @@
 const allResults = document.querySelectorAll(".search-result");
 
-allResults.forEach(async result => {
+allResults.forEach(result => {
   result.querySelector("._additionalInfo")?.remove();
 
-  const text = await retrieveText(result);
+  const text = retrieveText(result);
 
   addInfoRow(result, text);
 });
 
-async function retrieveText(result) {
-  const globalId = result.attributes.getNamedItem("data-global-id").value;
-  return `BOO '${globalId}'`;
+function getId(result) {
+  return result.attributes.getNamedItem("data-global-id").value;
+}
+
+function retrieveText(result) {
+  return window.localStorage.getItem(`house_info_${getId(result)}`);
+}
+
+function saveText(result, text) {
+  window.localStorage.setItem(`house_info_${getId(result)}`, text);
 }
 
 function addInfoRow(result, text) {
   const divElement = document.createElement("div");
-  divElement.className = "_additionalInfo text";
+  divElement.id = `fda_info_${getId(result)}`;
+
+  divElement.className = "_additionalInfo infoRow";
+
+  const textContainer = document.createElement("div");
+  textContainer.className = "_textContainer";
 
   const textElement = document.createElement("p");
+  textElement.className = "infoText";
   textElement.innerText = text;
-  divElement.appendChild(textElement);
+  textContainer.appendChild(textElement);
+  divElement.appendChild(textContainer);
+
+  const textArea = document.createElement("textarea");
+  textArea.className = "infoTextarea hidden";
+  textArea.value = text;
+  textContainer.appendChild(textArea);
+  divElement.appendChild(textContainer);
 
   const editButton = document.createElement("button");
-  editButton.onClick
+  textElement.className = "editButton";
+
+  if (text === null) {
+    editButton.innerText = "Notitie toevoegen";
+  } else {
+    editButton.innerText = "Aanpassen";
+  }
+
+  editButton.onclick = (e) => {
+    e.preventDefault();
+
+    editInfo(result, textElement, textArea);
+  };
+  divElement.appendChild(editButton);
+
   result.appendChild(divElement);
+}
+
+function editInfo(result, textElement, textArea) {
+  if (textArea.classList.contains("hidden")) {
+    const text = textElement.innerText;
+    textArea.value = text;
+
+    textArea.classList.remove("hidden");
+    textElement.classList.add("hidden");
+  } else {
+    const text = textArea.value;
+    textElement.innerText = text;
+
+    textArea.classList.add("hidden");
+    textElement.classList.remove("hidden");
+
+    saveText(result, text);
+  }
+
+
 }
